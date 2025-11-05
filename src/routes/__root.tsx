264a5eb428c0411665d6a8e3ef63946c2ddf7464
jxom <7336481+jxom@users.noreply.github.com>
2025-11-05 17:53:06 +1100

    refactor: isolate

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -40,9 +40,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 				<HeadContent />
 			</head>
 			<body className="antialiased">
-				<header className="pl-16 pt-12 flex items-center gap-2">
-					<img src="/icons/watermark.svg" alt="Tempo" className="w-26" />
-				</header>
 				<WagmiProvider config={config}>
 					<PersistQueryClientProvider
 						client={queryClient}
@@ -65,37 +62,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 					/>
 				)}
 				<Scripts />
-				<footer className="fixed bottom-0 left-0 right-0 bg-background-primary p-4 pb-10">
-					<ul className="flex items-center justify-center gap-4 text-[#7B7B7B] text-base *:hover:text-white transition-colors">
-						<li>
-							<a
-								href="https://tempo.xyz"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								About
-							</a>
-						</li>
-						<li>
-							<a
-								href="https://docs.tempo.xyz"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								Documentation
-							</a>
-						</li>
-						<li>
-							<a
-								href="https://github.com/tempoxyz"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								GitHub
-							</a>
-						</li>
-					</ul>
-				</footer>
 			</body>
 		</html>
 	)
--- src/routes/explore.tsx
+++ src/routes/explore.tsx
@@ -15,5 +15,43 @@ export const Route = createFileRoute('/explore')({
 
 // TODO: search bar, bg, footer, etc
 export function Layout() {
-	return <Outlet />
+	return (
+		<>
+			<header className="pl-16 pt-12 flex items-center gap-2">
+				<img src="/icons/watermark.svg" alt="Tempo" className="w-26" />
+			</header>
+			<Outlet />
+			<footer className="fixed bottom-0 left-0 right-0 bg-background-primary p-4 pb-10">
+				<ul className="flex items-center justify-center gap-4 text-[#7B7B7B] text-base *:hover:text-white transition-colors">
+					<li>
+						<a
+							href="https://tempo.xyz"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							About
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://docs.tempo.xyz"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Documentation
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://github.com/tempoxyz"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							GitHub
+						</a>
+					</li>
+				</ul>
+			</footer>
+		</>
+	)
 }
