5a99ae7860f200b1e0247fdcc62df63489e21173
o-az <omaraziz.dev@proton.me>
2025-11-12 07:49:12 -0800

    chore: ignore giant type gen file

--- apps/explorer/env.d.ts
+++ apps/explorer/env.d.ts
@@ -1,5 +1,13 @@
+interface EnvironmentVariables {
+	readonly INDEXSUPPLY_API_KEY: string
+}
+
 interface ImportMetaEnv extends Cloudflare.Env {}
 
 interface ImportMeta {
 	readonly env: ImportMetaEnv
+}
+
+declare namespace NodeJS {
+	interface ProcessEnv extends EnvironmentVariables {}
 }
