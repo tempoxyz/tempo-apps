a5399ae91f032e97d61597fc78ff1a043017f486
Pierre Bertet <hi@bpier.re>
2025-11-14 23:28:09 +0000

    feat: demo links (#25)
    * demo links
    
    * check

--- apps/explorer/src/components/Footer.tsx
+++ apps/explorer/src/components/Footer.tsx
@@ -1,35 +1,71 @@
+import { Link as RouterLink } from '@tanstack/react-router'
+import { useBlock } from 'wagmi'
+
+function DemoLinks() {
+	const { data: block } = useBlock()
+	const txes = block?.transactions.slice(0, 2)
+	return (
+		<>
+			<Footer.Link
+				to="/account/$address"
+				params={{ address: '0x5bc1473610754a5ca10749552b119df90c1a1877' }}
+			>
+				Account
+			</Footer.Link>
+			{Array.from({ length: 2 }, (_, index) => {
+				const hash = txes?.[index]
+				const label = `Receipt ${index + 1}`
+				const key = hash ?? String(index)
+				return hash ? (
+					<Footer.Link to="/tx/$hash" params={{ hash }} key={key}>
+						{label}
+					</Footer.Link>
+				) : (
+					<span className="select-none opacity-50" key={key}>
+						{label}
+					</span>
+				)
+			})}
+		</>
+	)
+}
+
 export function Footer() {
 	return (
 		<footer className="pt-[24px] pb-[48px] relative z-1">
 			<ul className="flex items-center justify-center gap-[24px] text-[15px] text-base-content-secondary">
-				<Footer.Link href="https://tempo.xyz">About</Footer.Link>
-				<Footer.Link href="https://docs.tempo.xyz">Documentation</Footer.Link>
-				<Footer.Link href="https://github.com/tempoxyz">GitHub</Footer.Link>
+				{/* <Footer.Link to="https://tempo.xyz" external>About</Footer.Link> */}
+				{/* <Footer.Link to="https://docs.tempo.xyz" external>Documentation</Footer.Link> */}
+				{/* <Footer.Link to="https://github.com/tempoxyz" external>GitHub</Footer.Link> */}
+				<DemoLinks />
 			</ul>
 		</footer>
 	)
 }
 
 export namespace Footer {
-	export function Link({ href, children }: Link.Props) {
+	export function Link({ to, params, children, external }: Link.Props) {
 		return (
 			<li className="flex">
-				<a
-					href={href}
-					target="_blank"
-					rel="noopener noreferrer"
-					className="hover:text-primary transition-[color] press-down"
+				<RouterLink
+					to={to}
+					params={params}
+					className="press-down"
+					target={external ? '_blank' : undefined}
+					rel={external ? 'noopener noreferrer' : undefined}
 				>
 					{children}
-				</a>
+				</RouterLink>
 			</li>
 		)
 	}
 
 	export namespace Link {
 		export interface Props {
-			href: string
+			to: string
+			params?: Record<string, string>
 			children: React.ReactNode
+			external?: boolean
 		}
 	}
 }
