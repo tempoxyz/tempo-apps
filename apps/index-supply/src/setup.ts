e36648fa70630690f07a3fc2cf0e30786f1a3de2
o-az <omaraziz.dev@proton.me>
2025-12-04 23:00:34 -0800

    fix: typecheck

--- apps/index-supply/src/setup.ts
+++ apps/index-supply/src/setup.ts
@@ -1,9 +1,7 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { csrf } from 'hono/csrf'
-import { showRoutes } from 'hono/dev'
 import { HTTPException } from 'hono/http-exception'
-import { logger } from 'hono/logger'
 import { prettyJSON } from 'hono/pretty-json'
 import { timeout } from 'hono/timeout'
 
@@ -16,11 +14,6 @@ app.use(
 	'*',
 	cors({ origin: '*', allowMethods: ['GET', 'OPTIONS', 'POST', 'HEAD'] }),
 )
-app.use('*', async (context, next) => {
-	logger()
-	if (context.env.LOGGING === 'verbose') showRoutes(app, { verbose: true })
-	await next()
-})
 
 app.notFound((context) => {
 	throw new HTTPException(404, {
