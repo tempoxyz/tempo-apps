c4586df9eb26e88391382d4d9aca71687c690483
o-az <omaraziz.dev@proton.me>
2025-12-09 15:25:38 -0800

    feat: smart contracts verification service

---
+++ apps/contract-verification/src/docs.tsx
@@ -0,0 +1,32 @@
+import { html, raw } from 'hono/html'
+
+const scalarConfig = {
+	slug: 'contract-verification',
+	hideModels: true,
+	hideClientButton: true,
+	url: '/schema/openapi.json',
+	showDeveloperTools: 'never',
+	documentDownloadType: 'json',
+	operationTitleSource: 'path',
+	title: 'Contract Verification API Reference',
+	proxyUrl: 'https://proxy.scalar.com',
+	favicon: 'https://contracts.tempo.xyz/favicon.ico',
+}
+
+export const Docs = () => {
+	return (
+		<html lang="en">
+			<head>
+				<title>Contract Verification API</title>
+				<meta charset="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</head>
+			<body>
+				{/** biome-ignore lint/correctness/useUniqueElementIds: _ */}
+				<main id="app"></main>
+				<script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+				<script>{html /* jsx */`Scalar.createApiReference('#app', ${raw(JSON.stringify(scalarConfig))})`}</script>
+			</body>
+		</html>
+	)
+}
