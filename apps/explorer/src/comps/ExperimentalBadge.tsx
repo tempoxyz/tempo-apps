c049b1fd3d382aa512e3efb6db3a159cd52677ae
o-az <omaraziz.dev@proton.me>
2025-12-14 21:31:51 -0800

    feat: experimental badge

---
+++ apps/explorer/src/comps/ExperimentalBadge.tsx
@@ -0,0 +1,22 @@
+import { cx } from '#cva.config.ts'
+
+export function ExperimentalBadge(props: ExperimentalBadge.Props) {
+	const { className } = props
+
+	return (
+		<div
+			className={cx(
+				'text-[11px] font-normal bg-base-alt text-base-content rounded-md px-[6px] py-[2px]',
+				className,
+			)}
+		>
+			Experimental
+		</div>
+	)
+}
+
+export declare namespace ExperimentalBadge {
+	type Props = {
+		className?: string
+	}
+}
