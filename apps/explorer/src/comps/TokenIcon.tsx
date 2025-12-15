cec4ac22f642d786deae2d8ca285341231a19109
o-az <omaraziz.dev@proton.me>
2025-12-15 14:03:19 -0800

    fix: a11y

--- apps/explorer/src/comps/TokenIcon.tsx
+++ apps/explorer/src/comps/TokenIcon.tsx
@@ -4,11 +4,11 @@ import { cx } from '#cva.config.ts'
 const TOKENLIST_BASE_URL = 'https://tokenlist.tempo.xyz/icon/42429'
 
 export function TokenIcon(props: TokenIcon.Props) {
-	const { address, name, className } = props
+	const { address, className } = props
 	return (
 		<img
 			src={`${TOKENLIST_BASE_URL}/${address}`}
-			alt={name ?? address}
+			alt=''
 			className={cx('size-4 rounded-full shrink-0', className)}
 			onError={(e) => {
 				e.currentTarget.style.display = 'none'
