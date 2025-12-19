cae7b2e170a1215692086fb430149b922db63559
o-az <omaraziz.dev@proton.me>
2025-12-19 04:41:54 -0800

    chore: cleanup

--- apps/explorer/src/routes/api/webauthn/$.ts
+++ apps/explorer/src/routes/api/webauthn/$.ts
@@ -5,16 +5,11 @@ import { Handler, Kv } from 'tempo.ts/server'
 export const Route = createFileRoute('/api/webauthn/$')({
 	server: {
 		handlers: {
-			GET: ({ request }) => {
-				const url = new URL(request.url)
-				const path = url.pathname
-
-				console.info('webauthn request', path)
-				return Handler.keyManager({
+			GET: ({ request }) =>
+				Handler.keyManager({
 					path: '/api/webauthn',
 					kv: Kv.cloudflare(env.EXPLORER_PASSKEY_STORE),
-				}).fetch(request)
-			},
+				}).fetch(request),
 		},
 	},
 })
