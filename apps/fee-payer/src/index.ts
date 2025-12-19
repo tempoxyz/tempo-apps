92d00c91194fc2e716494b440d698dc9fd0f9120
struong <10735730+struong@users.noreply.github.com>
2025-12-19 11:43:50 -0500

    feat: add devnet fee payer (#328)
    * feat: TEMPO_ENV
    
    * feat: devnet fee-payer
    
    * deploy devnet fee-payer in CI
    
    * fix: correct devnet URL
    
    ---------
    
    Co-authored-by: Steven Truong <struong@users.noreply.github.com>

--- apps/fee-payer/src/index.ts
+++ apps/fee-payer/src/index.ts
@@ -2,7 +2,7 @@ import { env } from 'cloudflare:workers'
 import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-import { tempo } from 'tempo.ts/chains'
+import { tempoDevnet, tempoTestnet } from 'tempo.ts/chains'
 import { Handler } from 'tempo.ts/server'
 import { http } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
@@ -11,6 +11,7 @@ import { rateLimitMiddleware } from './lib/rate-limit.js'
 import { getUsage } from './lib/usage.js'
 
 const app = new Hono()
+const tempoChain = env.TEMPO_ENV === 'devnet' ? tempoDevnet : tempoTestnet
 
 app.use(
 	'*',
@@ -53,7 +54,9 @@ app.get(
 app.all('*', rateLimitMiddleware, async (c) => {
 	const handler = Handler.feePayer({
 		account: privateKeyToAccount(env.SPONSOR_PRIVATE_KEY as `0x${string}`),
-		chain: tempo({ feeToken: '0x20c0000000000000000000000000000000000001' }),
+		chain: tempoChain({
+			feeToken: '0x20c0000000000000000000000000000000000001',
+		}),
 		transport: http(env.TEMPO_RPC_URL),
 		async onRequest(request) {
 			console.log(`Sponsoring transaction: ${request.method}`)
