c7a840be957cc1f4f6c1ff1925fdffa8d1cb8aee
o-az <omaraziz.dev@proton.me>
2025-12-25 13:11:25 -0800

    chore: update deps and enhance pnpm (#343)

---
+++ scripts/clean.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# remove all node_modules, recursively
+/usr/bin/find . -name "node_modules" -type d -exec rm -rf {} +
+
+# remove all dist, recursively
+/usr/bin/find . -name "dist" -type d -exec rm -rf {} +
+
+# remove all build, recursively
+/usr/bin/find . -name "build" -type d -exec rm -rf {} +
