2321b2f23ea119469575cbe6f72ea0b8bb9dc8a1
o-az <omaraziz.dev@proton.me>
2025-12-31 21:34:42 -0800

    chore: test update

--- apps/fee-payer/vitest.config.ts
+++ apps/fee-payer/vitest.config.ts
@@ -20,8 +20,8 @@ const rpcUrl = (() => {
 
 export default defineWorkersConfig({
 	test: {
-		include: ['src/**/e2e.test.ts'],
-		globalSetup: [join(__dirname, './src/test/setup.global.ts')],
+		include: ['src/**/e2e.test.ts', 'test/**/*.test.ts'],
+		globalSetup: [join(__dirname, './test/setup.global.ts')],
 		poolOptions: {
 			workers: {
 				wrangler: { configPath: './wrangler.jsonc' },
