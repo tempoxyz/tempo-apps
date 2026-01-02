d7579917bda33fad429ab859c0104eedf66836c3
o-az <omaraziz.dev@proton.me>
2026-01-02 09:56:21 -0800

    feat: introduce explorer devnet environment

--- apps/explorer/vite.config.ts
+++ apps/explorer/vite.config.ts
@@ -4,13 +4,17 @@ import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart as tanstack } from '@tanstack/react-start/plugin/vite'
 import react from '@vitejs/plugin-react'
 import Icons from 'unplugin-icons/vite'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv, type Plugin } from 'vite'
 import vitePluginChromiumDevTools from 'vite-plugin-devtools-json'
 
 const [, , , ...args] = process.argv
 
 export default defineConfig((config) => {
+  if (config.mode === 'devnet') process.env.CLOUDFLARE_ENV = 'devnet'
+	if (config.mode === 'testnet') process.env.CLOUDFLARE_ENV = 'testnet'
+
 	const env = loadEnv(config.mode, process.cwd(), '')
+
 	const showDevtools = env.VITE_ENABLE_DEVTOOLS !== 'false'
 
 	const lastPort = (() => {
@@ -22,37 +26,13 @@ export default defineConfig((config) => {
 	const allowedHosts = env.ALLOWED_HOSTS?.split(',') ?? []
 
 	return {
-		define: {
-			__BASE_URL__: JSON.stringify(
-				env.VITE_BASE_URL
-					? env.VITE_BASE_URL
-					: config.mode === 'development'
-						? `http://localhost:${port}`
-						: (env.VITE_BASE_URL ?? ''),
-			),
-			__BUILD_VERSION__: JSON.stringify(
-				env.CF_PAGES_COMMIT_SHA?.slice(0, 8) ?? Date.now().toString(),
-			),
-		},
 		plugins: [
-			{
-				// rolldown doesn't support interpolations in alias
-				// replacements so we use a custom resolver instead
-				name: 'explorer-aliases',
-				resolveId(id) {
-					if (id.startsWith('#tanstack')) return
-					if (id.startsWith('#'))
-						return this.resolve(`${__dirname}/src/${id.slice(1)}`)
-				},
-			},
+			vitePluginAlias(),
 			showDevtools && devtools(),
 			showDevtools && vitePluginChromiumDevTools(),
 			cloudflare({ viteEnvironment: { name: 'ssr' } }),
 			tailwind(),
-			Icons({
-				compiler: 'jsx',
-				jsx: 'react',
-			}),
+			Icons({ compiler: 'jsx', jsx: 'react' }),
 			tanstack({
 				srcDirectory: './src',
 				start: { entry: './src/index.start.ts' },
@@ -82,5 +62,30 @@ export default defineConfig((config) => {
 				},
 			},
 		},
+		define: {
+			__BASE_URL__: JSON.stringify(
+				env.VITE_BASE_URL
+					? env.VITE_BASE_URL
+					: config.mode === 'development'
+						? `http://localhost:${port}`
+						: (env.VITE_BASE_URL ?? ''),
+			),
+			__BUILD_VERSION__: JSON.stringify(
+				env.CF_PAGES_COMMIT_SHA?.slice(0, 8) ?? Date.now().toString(),
+			),
+		},
 	}
 })
+
+function vitePluginAlias(): Plugin {
+	return {
+		// rolldown doesn't support interpolations in alias
+		// replacements so we use a custom resolver instead
+		name: 'explorer-aliases',
+		resolveId(id) {
+			if (id.startsWith('#tanstack')) return
+			if (!id.startsWith('#')) return
+			return this.resolve(`${__dirname}/src/${id.slice(1)}`)
+		},
+	}
+}
