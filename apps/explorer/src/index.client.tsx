c68ab89013520cafde31c09f4609d6192420686a
o-az <omaraziz.dev@proton.me>
2026-01-05 13:50:09 -0800

    chore: save

--- apps/explorer/src/index.client.tsx
+++ apps/explorer/src/index.client.tsx
@@ -4,7 +4,7 @@ import posthog from 'posthog-js'
 import * as React from 'react'
 import { hydrateRoot } from 'react-dom/client'
 
-posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_API_KEY, {
+posthog.init(import.meta.env.VITE_POSTHOG_API_KEY, {
 	ui_host: 'https://us.posthog.com',
 	api_host: 'https://o11y.tempo.xyz',
 	enable_recording_console_log: true,
