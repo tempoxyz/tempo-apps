419e5cb3606bc16c4298d2a1d41ed481ac550376
Pierre Bertet <hi@bpier.re>
2026-01-08 01:20:40 +0000

    fix webauthn

--- apps/explorer/src/routes/api/webauthn/$.ts
+++ apps/explorer/src/routes/api/webauthn/$.ts
@@ -2,14 +2,17 @@ import { env } from 'cloudflare:workers'
 import { createFileRoute } from '@tanstack/react-router'
 import { Handler, Kv } from 'tempo.ts/server'
 
+const handler = ({ request }: { request: Request }) =>
+	Handler.keyManager({
+		path: '/api/webauthn',
+		kv: Kv.cloudflare(env.EXPLORER_PASSKEY_STORE),
+	}).fetch(request)
+
 export const Route = createFileRoute('/api/webauthn/$')({
 	server: {
 		handlers: {
-			GET: ({ request }) =>
-				Handler.keyManager({
-					path: '/api/webauthn',
-					kv: Kv.cloudflare(env.EXPLORER_PASSKEY_STORE),
-				}).fetch(request),
+			GET: handler,
+			POST: handler,
 		},
 	},
 })
