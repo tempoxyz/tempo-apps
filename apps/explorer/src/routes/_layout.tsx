738758298ca62cd40aa1957db5682e7bec15cf51
Jennifer <jenpaff0@gmail.com>
2026-01-08 18:50:44 +0000

    Show moderato info banner (#381)
    * Show moderato info banner
    
    ---------
    
    Co-authored-by: Pierre Bertet <hi@bpier.re>

--- apps/explorer/src/routes/_layout.tsx
+++ apps/explorer/src/routes/_layout.tsx
@@ -4,6 +4,7 @@ import { Footer } from '#comps/Footer'
 import { Header } from '#comps/Header'
 import { Sphere } from '#comps/Sphere'
 import { fetchLatestBlock } from '#lib/server/latest-block.server.ts'
+import TriangleAlert from '~icons/lucide/triangle-alert'
 
 export const Route = createFileRoute('/_layout')({
 	component: RouteComponent,
@@ -33,6 +34,26 @@ export function Layout(props: Layout.Props) {
 	const isReceipt = Boolean(matchRoute({ to: '/receipt/$hash', fuzzy: true }))
 	return (
 		<div className="flex min-h-dvh flex-col print:block print:min-h-0">
+			<div className="bg-warning-background border-warning px-[32px] py-[8px] text-sm text-warning text-center">
+				<TriangleAlert className="size-4 inline mr-[4px] relative top-[-1px]" />
+				<span className="">
+					<strong>Testnet migration:</strong> We've launched a new testnet (Moderato).
+						The old testnet (Andantino) will be deprecated on{' '}
+					<time dateTime="2026-03-08" title="March 8th, 2026">
+						March 8th
+					</time>
+					.{' '}
+					<a
+						href="https://docs.tempo.xyz/testnet-migration"
+						className="underline press-down-inline"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Checkout our docs 
+					</a>{' '}
+					for more details.
+				</span>
+			</div>
 			<div className={`relative z-2 ${isReceipt ? 'print:hidden' : ''}`}>
 				<Header initialBlockNumber={blockNumber} />
 			</div>
