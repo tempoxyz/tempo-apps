6800fdb64f829c55de9374aa6805900adc930d9f
Pierre Bertet <hi@bpier.re>
2026-01-14 02:09:25 +0000

    app wip

---
+++ apps/app/src/comps/Layout.tsx
@@ -0,0 +1,42 @@
+import type { PropsWithChildren } from 'react'
+
+export function Layout(props: PropsWithChildren) {
+	return <main className="mx-auto flex h-full max-lg:flex-col" {...props} />
+}
+
+export namespace Layout {
+	export function Hero(props: PropsWithChildren) {
+		return (
+			<div
+				className="fixed inset-2 w-[45vw] max-lg:relative max-lg:inset-0 max-lg:w-full max-lg:h-auto"
+				{...props}
+			/>
+		)
+	}
+
+	export function Content(props: PropsWithChildren) {
+		return (
+			<div
+				className="ml-[calc(45vw+16px)] flex w-full flex-1 flex-col py-3 max-md:py-2 max-lg:ml-0"
+				{...props}
+			>
+				<div className="mx-auto flex w-full max-w-2xl flex-1 flex-col px-3 max-sm:px-2">
+					{props.children}
+				</div>
+			</div>
+		)
+	}
+
+	export function Header(props: {
+		left?: React.ReactNode
+		right?: React.ReactNode
+	}) {
+		const { left, right } = props
+		return (
+			<div className="flex items-center justify-between">
+				<div className="min-lg:opacity-0">{left}</div>
+				{right}
+			</div>
+		)
+	}
+}
---
+++ apps/app/src/routes/__root.tsx
@@ -0,0 +1,81 @@
+import { TanStackDevtools } from '@tanstack/react-devtools'
+import { type QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {
+	createRootRouteWithContext,
+	HeadContent,
+	Outlet,
+	Scripts,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import * as React from 'react'
+import { deserialize, type State, WagmiProvider } from 'wagmi'
+import { getWagmiConfig, getWagmiStateSSR } from '#wagmi.config.ts'
+import css from './styles.css?url'
+
+export const Route = createRootRouteWithContext<{
+	queryClient: QueryClient
+}>()({
+	loader: () => getWagmiStateSSR(),
+	head: () => ({
+		meta: [
+			{ charSet: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ title: 'App ⋅ Tempo' },
+			{ name: 'og:title', content: 'App ⋅ Tempo' },
+			{ name: 'description', content: 'Tempo App' },
+			{ name: 'og:description', content: 'Tempo App' },
+		],
+		links: [
+			{ rel: 'stylesheet', href: css },
+			{
+				rel: 'icon',
+				type: 'image/svg+xml',
+				href: '/favicon-light.svg',
+				media: '(prefers-color-scheme: light)',
+			},
+			{
+				rel: 'icon',
+				type: 'image/svg+xml',
+				href: '/favicon-dark.svg',
+				media: '(prefers-color-scheme: dark)',
+			},
+		],
+	}),
+	component: RootComponent,
+})
+
+function RootComponent() {
+	const { queryClient } = Route.useRouteContext()
+	const [config] = React.useState(() => getWagmiConfig())
+	const wagmiState = Route.useLoaderData({ select: deserialize<State> })
+
+	return (
+		<html lang="en" className="scheme-light-dark scrollbar-gutter-stable">
+			<head>
+				<HeadContent />
+			</head>
+			<body className="antialiased">
+				<WagmiProvider config={config} initialState={wagmiState}>
+					<QueryClientProvider client={queryClient}>
+						<Outlet />
+						{import.meta.env.MODE === 'development' &&
+							import.meta.env.VITE_ENABLE_DEVTOOLS === 'true' && (
+								<TanStackDevtools
+									config={{ position: 'bottom-right' }}
+									plugins={[
+										{ name: 'Tanstack Query', render: <ReactQueryDevtools /> },
+										{
+											name: 'Tanstack Router',
+											render: <TanStackRouterDevtoolsPanel />,
+										},
+									]}
+								/>
+							)}
+					</QueryClientProvider>
+				</WagmiProvider>
+				<Scripts />
+			</body>
+		</html>
+	)
+}
