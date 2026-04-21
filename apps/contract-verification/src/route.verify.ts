50758361395c447c223c17266a32aa8c49ae9f72
o-az <omaraziz.dev@proton.me>
2026-04-21 06:30:30 -0700

    feedback

--- apps/contract-verification/src/route.verify.ts
+++ apps/contract-verification/src/route.verify.ts
@@ -111,7 +111,7 @@ verifyRoute
 			'param',
 			z.object({
 				chainId: zChainId(),
-				address: zAddress({ strict: false }),
+				address: zAddress(),
 			}),
 			(value, context) => {
 				if (!value.success)
@@ -119,7 +119,7 @@ verifyRoute
 						context,
 						400,
 						'invalid_chain_id_or_address',
-						`Invalid chainId or address: ${JSON.stringify(value.data)} - ${z.prettifyError(value.error)}`,
+						`Invalid chainId or address - ${z.prettifyError(value.error)}`,
 					)
 			},
 		),
