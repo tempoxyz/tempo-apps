23771de639c9142fbeef1b7a3fc95bfea17d2537
Emma Jamieson-Hoare <ejamieson19@gmail.com>
2026-05-26 11:23:09 +0100

    refactor(perf): remove public perf app

--- apps/perf/src/index.server.ts
+++
@@ -1,9 +0,0 @@
-import handler, { createServerEntry } from '@tanstack/react-start/server-entry'
-
-const serverEntry = createServerEntry({
-	fetch: (request, opts) => handler.fetch(request, opts),
-})
-
-export default {
-	fetch: (request: Request) => serverEntry.fetch(request, undefined),
-}
